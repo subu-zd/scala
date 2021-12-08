3b50db1618263112072ae24979be1d093c41cbd8
Subu <subu.kazhuparambil@zendesk.com>
2021-12-08 13:20:06 +0000

    JVM threads

--- src/main/scala/lectures/part7_concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/part7_concurrency/ThreadCommunication.scala
@@ -5,13 +5,13 @@ import scala.util.Random
 
 object ThreadCommunication extends App {
   /*
-  * the producer-consumer problem
-  *
-  * producer -> [ x ] -> consumer
-  * p: sets value inside the container
-  * c: extracts value out of the container
-  * p & c are running in parallel at the same time
-  * */
+   * the producer-consumer problem
+   *
+   * producer -> [ x ] -> consumer
+   * p: sets value inside the container
+   * c: extracts value out of the container
+   * p & c are running in parallel at the same time
+   * */
 
   class SimpleContainer {
     private var value: Int = 0
@@ -33,7 +33,7 @@ object ThreadCommunication extends App {
     val consumer = new Thread(() => {
       println("[consumer] waiting ...")
 
-      while(container.isEmpty) {
+      while (container.isEmpty) {
         println("[consumer] actively waiting ...")
       }
 
@@ -90,13 +90,13 @@ object ThreadCommunication extends App {
   // complicated producer-consumer
 
   /*
-  * producer -> [ ? ? ? ] -> consumer extracts any value that is new
-  *
-  * a caveat -> both the producer or consumer may block each other
-  * e.g. the buffer is full  i.e the producer has produced enough values to fill the buffer
-  * in that case, the producer must block until the consumer has finished extracting some values out and vice-verse,
-  * i.e. if the consumer is fast and the buffer is empty, the consumer must block until the producer produces more
-  * */
+   * producer -> [ ? ? ? ] -> consumer extracts any value that is new
+   *
+   * a caveat -> both the producer or consumer may block each other
+   * e.g. the buffer is full  i.e the producer has produced enough values to fill the buffer
+   * in that case, the producer must block until the consumer has finished extracting some values out and vice-verse,
+   * i.e. if the consumer is fast and the buffer is empty, the consumer must block until the producer produces more
+   * */
 
   def prodConsLargeBuffer(): Unit = {
     val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
@@ -105,9 +105,9 @@ object ThreadCommunication extends App {
     val consumer = new Thread(() => {
       val random = new Random()
 
-      while(true) {
+      while (true) {
         buffer.synchronized {
-          if(buffer.isEmpty) {
+          if (buffer.isEmpty) {
             println("[consumer] buffer empty, waiting ...")
             buffer.wait()
           }
@@ -127,7 +127,7 @@ object ThreadCommunication extends App {
       val random = new Random()
       var i = 0
 
-      while(true) {
+      while (true) {
         buffer.synchronized {
           if (buffer.size == capacity) {
             println("[producer] buffer is full, waiting ...")
@@ -140,7 +140,7 @@ object ThreadCommunication extends App {
 
           buffer.notify()
 
-          i+=1
+          i += 1
         }
 
         Thread.sleep(random.nextInt(500))
@@ -151,29 +151,41 @@ object ThreadCommunication extends App {
     producer.start()
   }
 
-  prodConsLargeBuffer()
+//  prodConsLargeBuffer()
 
   /*
-  * Prod-Cons level 3
-  *
-  * producer1 -> [ ? ? ?] -> consumer1
-  * producer2 -> ... -> consumer2
-  * */
+   * Prod-Cons level 3
+   *
+   * producer1 -> [ ? ? ?] -> consumer1
+   * producer2 -> ... -> consumer2
+   * */
 
   class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
     override def run(): Unit = {
       val random = new Random()
 
-      while(true) {
+      while (true) {
         buffer.synchronized {
 
           /*
-          * producer produces value, two Cons are waiting
-          * producer.notify() will notify one of the consumers ... afterwards, the consumer will dequeue and notify the buffer
-          * the JVm will then notify the other consumer and it will try to dequeue which is WRONG!!!
-          * */
+           * producer produces value, two Cons are waiting
+           * producer.notify() will notify one of the consumers ... afterwards, the consumer will dequeue and notify the buffer
+           * the JVM will then notify the other consumer and it will try to dequeue which is WRONG!!!
+           * */
 
-          if(buffer.isEmpty) {
+          /* if(buffer.isEmpty) {
+            println(s"[consumer $id] buffer empty, waiting ...")
+            buffer.wait()
+          }
+
+          replacing the "if" block with a "while" block will help us tackle the above situation because
+          if the thread is able to dequeue, it means the buffer is not empty AND the thread is awake. with "if"
+          we were able to reach the dequeue point if the bugger was empty OR the thread was awake.
+
+          A similar change will take place in the Producer class.
+           */
+
+          while (buffer.isEmpty) {
             println(s"[consumer $id] buffer empty, waiting ...")
             buffer.wait()
           }
@@ -182,6 +194,8 @@ object ThreadCommunication extends App {
           val x = buffer.dequeue()
           println(s"[consumer $id] consumed " + x)
 
+          // buffer.notify() no longer has the same semantics anymore because with the introduction of multiple prods and cons
+          // it can notify a producer or another consumer and vice-versa
           buffer.notify()
         }
 
@@ -195,9 +209,9 @@ object ThreadCommunication extends App {
       val random = new Random()
       var i = 0
 
-      while(true) {
+      while (true) {
         buffer.synchronized {
-          if (buffer.size == capacity) {
+          while (buffer.size == capacity) {
             println(s"[producer $id] buffer is full, waiting ...")
             buffer.wait()
           }
@@ -208,11 +222,118 @@ object ThreadCommunication extends App {
 
           buffer.notify()
 
-          i+=1
+          i += 1
         }
 
         Thread.sleep(random.nextInt(500))
       }
     }
   }
- }
+
+  def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
+    (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
+  }
+
+//  multiProdCons(3, 3)
+
+  /*
+   * Exercises:
+   *
+   * 1) come up with an example where notifyAll() acts in a different way than notify()
+   * -> in the case of multi prods and cons, the behaviour will be the same for both because,
+   * -> at every iteration inside either the producer or consumer, they're all synchronising on buffer
+   * -> so even if there multiple prods and/or cons sleeping on buffer and we notifyAll(),
+   * -> then at the next iteration when all threads are awake, they'll again all synchronise at the buffer i.e. all threads will be blocked except one
+   *
+   * 2) create a deadlock
+   * 3) create a livelock (threads are active and not blocked but can't continue)
+   *
+   * */
+
+  // Q1
+  def testNotifyAll(): Unit = {
+    val bell = new Object
+
+    (1 to 10).foreach(i =>
+      new Thread(() => {
+        bell.synchronized {
+          println(s"[thread $i] waiting ...")
+          bell.wait()
+          println(s"[thread $i] hooray")
+        }
+      }).start()
+    )
+
+    new Thread(() => {
+      Thread.sleep(2000)
+      println("Announcer rock-n-roll")
+      bell.synchronized {
+        bell.notifyAll()
+//        bell.notify()
+
+        /*
+         * When using notify() in this case:
+         * - 10 threads are initially waiting
+         * - but after announcer, just one thread wakes up
+         * - other 9 threads are still blocked and waiting to be woken up. This won't happen since notify() just awakens one thread
+         * */
+      }
+    }).start()
+  }
+
+//  testNotifyAll()
+
+  // Q2
+
+  case class Friend(name: String) {
+    def bow(other: Friend) = {
+      this.synchronized {
+        println(s"$this: I am bowing to my friend $other")
+        other.rise(this)
+        println(s"$this: my friend $other has risen")
+      }
+    }
+
+    def rise(other: Friend) = {
+      this.synchronized(println(s"$this: I am rising to my friend, $other"))
+    }
+
+    var side = "right"
+
+    def switchSide() = {
+      if (side == "right") side = "left"
+      else side = "right"
+    }
+
+    def pass(other: Friend) = {
+      while (this.side == other.side) {
+        println(s"$this: please, $other, pass")
+        switchSide()
+        Thread.sleep(1000)
+      }
+    }
+  }
+
+  val f1 = Friend("f1")
+  val f2 = Friend("f2")
+
+//  new Thread(() => f1.bow(f2)).start()
+//  new Thread(() => f2.bow(f1)).start()
+
+  /*
+   * The first thread blocks f1's lock as f1 bows to f2. Then subsequently blocks f2's lock
+   * The second thread blocks f2's lock as f2 bows to f1. Then subsequent;y blocks f1's lock
+   *
+   * When the two threads act at the same time, they block their respective locks and then try to block each other's locks
+   * which are at this point already locked
+   *  */
+
+  // Q3 - Livelock
+
+  new Thread(() => f1.pass(f2)).start()
+  new Thread(() => f2.pass(f1)).start()
+}
